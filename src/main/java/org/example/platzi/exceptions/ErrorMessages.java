64a3cf156121526d2379ffb8154600a0af24145e
DavidS2602 <DavidS260202@proton.me>
2024-12-26 10:42:47 -0600

    Reformat: Messages exception

--- src/main/java/org/example/platzi/exceptions/ErrorMessages.java
+++ src/main/java/org/example/platzi/exceptions/ErrorMessages.java
@@ -2,8 +2,8 @@ package org.example.platzi.exceptions;
 
 public enum ErrorMessages {
 
-    STUDENT_NOT_FOUND("Student WITH dni %s not found"),
-    DUPLICATE_STUDENT("Student %s already exists"),
+    STUDENT_NOT_FOUND("Student with dni %s not found"),
+    DUPLICATE_STUDENT("Student with name %s and DNI $s already exists"),
     BOOK_NOT_FOUND("Book with ISBN %s not found"),
     DUPLICATE_BOOK("Book with DNI %s and name %s already exists"),
     DUPLICATE_LOAN("Loan already exists for Book ID: $s and student ID: $s"),
